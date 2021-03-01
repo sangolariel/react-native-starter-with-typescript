8d8cad00693ce2177e29f083de4b702a40c1fc67
sangnh <sangnh@futurify.vn>
2021-03-01 18:07:58 +0700

    add lib

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,24 +1,12 @@
-import { combineReducers } from 'redux'
-import themeReducer from './theme/reducer'
-import languageReducer from './language/reducer'
-import commonReducer from './common/reducer'
-import categoryReducer from './category/reducer'
-import authReducer from './auth/reducer'
-import productReducer from './product/reducer'
-import cartReducer from './cart/reducer'
+import {combineReducers} from 'redux';
+import commonReducer from './common/reducer';
 
 /**
  * Root reducer
  * @type {Reducer<any> | Reducer<any, AnyAction>}
  */
 const rootReducers = combineReducers({
-  theme: themeReducer,
-  language: languageReducer,
   common: commonReducer,
-  auth: authReducer,
-  category: categoryReducer,
-  product: productReducer,
-  cart: cartReducer,
 });
 
-export default rootReducers
+export default rootReducers;
--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -1,15 +1,9 @@
-import { all } from 'redux-saga/effects'
-import commonSaga from './common/saga'
-import languageSaga from './language/saga'
-import categorySaga from './category/saga'
-import themeSaga from './theme/saga'
-import authSaga from './auth/saga'
-import productSaga from './product/saga'
-import cartSaga from './cart/saga'
+import {all} from 'redux-saga/effects';
+import commonSaga from './common/saga';
 /**
  * Root saga
  * @returns {IterableIterator<AllEffect | GenericAllEffect<any> | *>}
  */
 export default function* rootSagas() {
-  yield all([themeSaga(), authSaga(), commonSaga(), languageSaga(), categorySaga(), productSaga(), cartSaga()])
+  yield all([commonSaga()]);
 }
