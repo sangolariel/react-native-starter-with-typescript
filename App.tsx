67bb9423c147a6785a5bfbefe1ddd2308684c878
sangnh <sangnh@futurify.vn>
2021-03-02 14:11:09 +0700

    svg

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {Home} from '~/theme/svg/bottomTab';
 
 const App = () => {
-  return <Text>App</Text>;
+  return (
+    <View style={styles.container}>
+      <Home />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
